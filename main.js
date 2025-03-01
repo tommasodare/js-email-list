eac8f96443fd346db25ff70e0eb66014debde718
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-01 13:14:24 +0100

    Exercise Done

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-.then(response => response.json())
-.then (data => {
-    console.log(data);
-})
-.catch(error => {
-    console.error(error);
-})
+const listEl = document.querySelector(".list")
+
+// Ciclo per 10 volte
+for (let index = 0; index < 10; index++) {
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then(response => response.json())
+        .then(data => {
+            //console.log(data.response)
+
+            const emailList = []
+            emailList.push(data.response)
+            //console.log(emailList);
+            
+            // Ciclo all'interno della lista vuota emailList riempita dalle 10 email random richiamate tramite fetch
+            for (let index = 0; index < emailList.length; index++) {
+                const thisEmail = emailList[index];
+                console.log(thisEmail);
+
+                listEl.innerHTML += `<li>${thisEmail}</li>`
+            }
+        })
+}
